ad53fd27d64d36564050058f548ac72eebd5f042
= <=>
2017-04-20 18:31:15 +0300

    Коммит молча

--- Views/DependencyTableView.xaml.cs
+++ Views/DependencyTableView.xaml.cs
@@ -14,14 +14,16 @@ namespace Troubleshooting.Views
     {
         public DependencyTableViewModel ViewModel { get; set; }
 
+        private static readonly FontFamily TimesNewRoman = new FontFamily("Times New Roman");
+
         public DependencyTableView(DependencyTableViewModel viewModel)
         {
             ViewModel = viewModel;
             InitializeComponent();
             for (int i = 0, count = viewModel.Count; i < count; i++)
-                DependencyDiagram.ColumnDefinitions.Add(new ColumnDefinition());
+                DependencyDiagram.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto});
             for (int i = 0, count = viewModel.TableDepth+1; i < count; i++)
-                DependencyDiagram.RowDefinitions.Add(new RowDefinition());
+                DependencyDiagram.RowDefinitions.Add(new RowDefinition{ Height = GridLength.Auto });
 
             AddMyTableToMetaGrid(viewModel,0,0,2);
             CreateDependencyTable(ViewModel);
@@ -46,18 +48,19 @@ namespace Troubleshooting.Views
             myGrid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(20)});
             myGrid.RowDefinitions.Add(new RowDefinition(){Height = GridLength.Auto});
 
-            myGrid.ColumnDefinitions.Add(new ColumnDefinition(){Width = GridLength.Auto});
-            myGrid.ColumnDefinitions.Add(new ColumnDefinition(){Width = GridLength.Auto});
+            myGrid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1,GridUnitType.Star)});
+            myGrid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1,GridUnitType.Star) });
 
             var minW = myTable.W.IndexOfMin();
             Button button = new Button
             {
                 Content = $"Z{myTable.Nodes[minW].Zindex + 1}",
-                FontFamily = new FontFamily("Times New Roman"),
+                FontFamily = TimesNewRoman,
                 FontSize = 14,
+                MinWidth = 28,
                 Margin= new Thickness(2,0,2,0),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
-                VerticalAlignment = VerticalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Center,
             };
             button.MouseEnter += (o, e) =>
             {
@@ -86,12 +89,12 @@ namespace Troubleshooting.Views
                 {
                     Data = new PathGeometry(new[]
                     {
-                        new PathFigure(new Point(0, 0), new[]
+                        new PathFigure(new Point(7, 0), new[]
                         {
-                            new LineSegment(new Point(0, 20), true),
-                            new LineSegment(new Point(7, 10), false),
-                            new LineSegment(new Point(0, 20), true),
-                            new LineSegment(new Point(-7, 10), true),
+                            new LineSegment(new Point(7, 20), true),
+                            new LineSegment(new Point(14, 10), false),
+                            new LineSegment(new Point(7, 20), true),
+                            new LineSegment(new Point(0, 10), true),
                         }, false),
                     }),
                     Stroke = Brushes.Black,
@@ -101,22 +104,23 @@ namespace Troubleshooting.Views
                 TextBlock textBlock = new TextBlock()
                 {
                     Text = isWork.ToString(),
-                    FontFamily = new FontFamily("Times New Roman"),
+                    Margin = new Thickness(5, 0, 5, 0),
+                    FontFamily = TimesNewRoman,
                     FontSize = 14,
                 };
 
                 if (isWork == 0)
                 {
-                    myGrid.AddChildren(path, 0, 0, 1, 1, default(Color), false);
+                    myGrid.AddChildren(path, 1, 0, 1, 1, default(Color), false);
                     path.HorizontalAlignment = HorizontalAlignment.Left;
-                    myGrid.AddChildren(textBlock, 1, 0, 1, 1, default(Color), false);
+                    myGrid.AddChildren(textBlock, 0, 0, 1, 1, default(Color), false);
                     textBlock.HorizontalAlignment = HorizontalAlignment.Right;
                 }
                 if (isWork == 1)
                 {
-                    myGrid.AddChildren(path, 1, 0, 1, 1, default(Color), false);
+                    myGrid.AddChildren(path, 0, 0, 1, 1, default(Color), false);
                     path.HorizontalAlignment = HorizontalAlignment.Right;
-                    myGrid.AddChildren(textBlock, 0, 0, 1, 1, default(Color), false);
+                    myGrid.AddChildren(textBlock, 1, 0, 1, 1, default(Color), false);
                     textBlock.HorizontalAlignment = HorizontalAlignment.Left;
                 }
             }
@@ -136,40 +140,58 @@ namespace Troubleshooting.Views
 
             for (int i = 0, count = vm.Count + 4; i < count; i++)
                 DependencyTable.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-            for (int i = 0, count = vm.Count + 2; i < count; i++)
+            for (int i = 0, count = vm.Count + 3; i < count; i++)
                 DependencyTable.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
 
             DependencyTable.AddChildren(new TextBlock
             {
+                Text = vm.TableDescription,
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextAlignment = TextAlignment.Center
+            }, 0, 0, vm.Count + 4, 1, default(Color), false);
+
+            DependencyTable.AddChildren(new TextBlock
+            {
                 Text = "Zi",
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
                 Width = 30,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
                 TextAlignment = TextAlignment.Center
-            }, 0, 0);
+            }, 0, 1);
 
 
             DependencyTable.AddChildren(new TextBlock
             {
                 Text = "Состояние блока Si",
+                FontSize = 14,
+                FontFamily = TimesNewRoman,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center
-            }, 1, 0, vm.Count);
+            }, 1, 1, vm.Count);
             DependencyTable.AddChildren(new TextBlock
             {
                 Text = "Число\nсостояний",
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 TextAlignment = TextAlignment.Center,
                 Margin = new Thickness(5, 0, 5, 0)
-            }, vm.Count + 1, 0, 2);
+            }, vm.Count + 1, 1, 2);
             DependencyTable.AddChildren(new TextBlock
             {
                 Text = "Wi",
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
                 Width = 25,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
                 TextAlignment = TextAlignment.Center
-            }, vm.Count + 3, 0);
+            }, vm.Count + 3, 1);
 
             DependencyTable.AddChildren(new TextBlock { Text = "" }, 0, 1);
             for (int i = 0; i < vm.Count; i++)
@@ -178,6 +200,10 @@ namespace Troubleshooting.Views
                 var zindexTextBlock = new TextBlock
                 {
                     Text = $"{vm.Nodes[i].Zindex + 1}",
+                    FontFamily = TimesNewRoman,
+                    FontSize = 14,
+                    MinWidth = 25,
+                    VerticalAlignment = VerticalAlignment.Center,
                     TextAlignment = TextAlignment.Center
                 };
                 int zindexI = i;
@@ -191,19 +217,25 @@ namespace Troubleshooting.Views
                     vm.Nodes[zindexI].ParentMode = false;
                     vm.Nodes[zindexI].ChildMode = false;
                 };
-                DependencyTable.AddChildren(zindexTextBlock, i + 1, 1);
+                DependencyTable.AddChildren(zindexTextBlock, i + 1, 2);
             }
             DependencyTable.AddChildren(new TextBlock
             {
                 Text = "0",
-                HorizontalAlignment = HorizontalAlignment.Center
-            }, vm.Count + 1, 1);
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
+                HorizontalAlignment = HorizontalAlignment.Center, 
+                VerticalAlignment = VerticalAlignment.Center
+            }, vm.Count + 1, 2);
             DependencyTable.AddChildren(new TextBlock
             {
                 Text = "1",
-                HorizontalAlignment = HorizontalAlignment.Center
-            }, vm.Count + 2, 1);
-            DependencyTable.AddChildren(new TextBlock { Text = "" }, vm.Count + 3, 1);
+                FontFamily = TimesNewRoman,
+                FontSize = 14,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            }, vm.Count + 2, 2);
+            DependencyTable.AddChildren(new TextBlock { Text = "" }, vm.Count + 3, 2);
 
             var wIntexOfMin = vm.W.IndexOfMin();
             for (int i = 0; i < vm.Count; i++)
@@ -213,6 +245,8 @@ namespace Troubleshooting.Views
                 var zindexTextBlock = new TextBlock
                 {
                     Text = $"Z{vm.Nodes[i].Zindex + 1}",
+                    FontSize = 14,
+                    FontFamily = TimesNewRoman,
                     Margin = new Thickness(3, 0, 0, 0)
                 };
                 int zindexI = i;
@@ -227,7 +261,7 @@ namespace Troubleshooting.Views
                     vm.Nodes[zindexI].ChildMode = false;
                 };
 
-                DependencyTable.AddChildren(zindexTextBlock, 0, i + 2, 1, 1, color);
+                DependencyTable.AddChildren(zindexTextBlock, 0, i + 3, 1, 1, color);
 
 
                 for (int j = 0; j < vm.Count; j++)
@@ -235,9 +269,12 @@ namespace Troubleshooting.Views
                     var dependTextBlock = new TextBlock
                     {
                         Text = $"{vm.IsWorkMatrix[i, j]}",
-                        MinWidth = 20,
-                        HorizontalAlignment = HorizontalAlignment.Center,
-                        Background = new SolidColorBrush(vm.IsWorkMatrix[i, j] == 1
+                        FontFamily = TimesNewRoman,
+                        FontSize = 14,
+                        TextAlignment = TextAlignment.Center,
+                        MinWidth = 25,
+                        Margin = new Thickness(1,0.5,0.5,1),
+                        Background = new SolidColorBrush( vm.IsWorkMatrix[i, j] == 1
                             ? Color.Add(Color.FromArgb(255, 245, 245, 220), color) : color)
                     };
 
@@ -257,26 +294,35 @@ namespace Troubleshooting.Views
                         };
                     }
 
-                    DependencyTable.AddChildren(dependTextBlock, j + 1, i + 2);
+                    DependencyTable.AddChildren(dependTextBlock, j + 1, i + 3);
                 }
                 DependencyTable.AddChildren(new TextBlock
                 {
                     Text = $"{vm.Zeros[i]}",
+                    FontFamily = TimesNewRoman,
+                    FontSize = 14,
                     HorizontalAlignment = HorizontalAlignment.Right,
+                    VerticalAlignment = VerticalAlignment.Center,
                     Margin = new Thickness(0, 0, 3, 0)
-                }, vm.Count + 1, i + 2, 1, 1, color);
+                }, vm.Count + 1, i + 3, 1, 1, color);
                 DependencyTable.AddChildren(new TextBlock
                 {
                     Text = $"{vm.Ones[i]}",
+                    FontFamily = TimesNewRoman,
+                    FontSize = 14,
                     HorizontalAlignment = HorizontalAlignment.Right,
+                    VerticalAlignment = VerticalAlignment.Center,
                     Margin = new Thickness(0, 0, 3, 0)
-                }, vm.Count + 2, i + 2, 1, 1, color);
+                }, vm.Count + 2, i + 3, 1, 1, color);
                 DependencyTable.AddChildren(new TextBlock
                 {
                     Text = $"{vm.W[i]}",
+                    FontSize = 14,
+                    FontFamily = TimesNewRoman,
                     HorizontalAlignment = HorizontalAlignment.Right,
+                    VerticalAlignment = VerticalAlignment.Center,
                     Margin = new Thickness(0, 0, 3, 0)
-                }, vm.Count + 3, i + 2, 1, 1, color);
+                }, vm.Count + 3, i + 3, 1, 1, color);
             }
         }
     }
